672ff8319f8272084c2d9a14f8f27766d33f1dba
kenji <kenjiogier@gmail.com>
2025-12-10 18:14:18 +0100

    Correction affichage menu burger en mode clair

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
     };
 
   return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+      <nav className={`navbar navbar-expand-lg ${isDark ? 'navbar-dark bg-dark' : 'navbar-light bg-light'} sticky-top`}>
           <div className="container-fluid">
           {/* Logo de la navbar */}
           <Link className="navbar-brand" to="/">
